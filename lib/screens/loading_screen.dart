806741162100f17e269b3c3c030a1a67e2c45685
Eduardo Rosas <lalorosas@outlook.com>
2020-05-23 17:07:47 -0500

    Get current location's current weather

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+import '../services/location.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +9,29 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    getWeatherData(latitude: location.latitude, longitude: location.longitude);
+  }
+
+  void getWeatherData({double latitude, double longitude}) async {
+    String url =
+        'http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=cebc832c85bac282dc33969a103af744';
+    var response = await get(url);
+    print(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
