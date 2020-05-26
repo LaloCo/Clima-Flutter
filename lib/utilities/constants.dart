b5294287dcc9cb1844d6f90355b3f01d3a8b021b
Eduardo Rosas <lalorosas@outlook.com>
2020-05-26 12:10:49 -0500

    Get typed name when popping screen

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -19,5 +19,20 @@ const kConditionTextStyle = TextStyle(
   fontSize: 100.0,
 );
 
+const kTextFieldStyle = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: 'Enter city name',
+  hintStyle: TextStyle(color: Colors.grey),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+    borderSide: BorderSide.none,
+  ),
+);
+
 const kOWApiKey = 'cebc832c85bac282dc33969a103af744';
 const kOWWeatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather';
